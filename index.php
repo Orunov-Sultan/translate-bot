a9e07e41125d5559cc6c6234c3ba1202e1e61780
Orunov Sultan <sultan.orunov@mail.ru>
2022-07-27 00:08:19 +0500

    ЗаписываеÑмÐ даннфые пользÑователя в базу еслÐ и выводим кнопки с языками

--- index.php
+++ index.php
@@ -10,6 +10,34 @@ $telegram = new Telegram(TOKEN);
 
 $chat_id = $telegram->ChatID() ?? '';
 $text = $telegram->Text() ?? '';
+$first_name = $telegram->FirstName();
+
+
+if ($text == '/start' || $text == 'start'){
+
+    $data = getChatId($chat_id);
+    if (empty($data)){
+        addChatId($chat_id, $first_name, 'en');
+        $check = 'en';
+    }else{
+        $check = $data['lang'];
+    }
+
+    $option = [
+        [
+            $telegram->buildInlineKeyBoardButton("en", '', 'en'),
+            $telegram->buildInlineKeyBoardButton("ru", '', 'ru')
+        ]
+    ];
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = [
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => "Оставьте отмеченный язык для перевода с него или выберите другой"
+    ];
+    $telegram->sendMessage($content);
+}
+
 
 file_put_contents(__DIR__.'/log.txt', print_r($telegram, 1));
 
