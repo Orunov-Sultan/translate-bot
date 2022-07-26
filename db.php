a2a95e1c0c66e3ededca0f2d71263f4ed255a516
Orunov Sultan <sultan.orunov@mail.ru>
2022-07-26 23:44:57 +0500

    ДобÐавил файл db.php с необходи¼ыми фуÑнÑкцÐутнкцÑиямиÐ

--- db.php
+++ db.php
@@ -1,1 +1,34 @@
 <?php
+
+$host = 'localhost';
+$user = 'ci32018_tr';
+$pass = '1RTTin2R';
+$db = 'ci32018_tr';
+
+$dsn = "mysql:host={$host};dbname={$db};charset=utf8";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+function getChatId($chat_id){
+    global $pdo;
+
+    $stmt = $pdo->prepare('SELECT * FROM chat WHERE chat_id = ?');
+    $stmt->execute([$chat_id]);
+    return $stmt->fetch();
+}
+
+function addChatId($chat_id, $first_name, $lang){
+    global $pdo;
+    $stmt = $pdo->prepare('INSERT INTO (chat_id, first_name, lang) VALUES (?, ?, ?)');
+    return $stmt->execute([$chat_id, $first_name, $lang]);
+}
+
+function updateChat($chat_id, $lang){
+    global $pdo;
+    $stmt = $pdo->prepare('UPDATE chat SET lang = ? WHERE chat_id = ?');
+    return $stmt->execute([$lang, $chat_id]);
+}
--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 require_once 'Telegram.php';
 require_once 'TelegramErrorLogger.php';
 require_once 'config.php';
+require_once 'db.php';
 
 //https://api.telegram.org/bot(BOT_TOKEN)/setWebhook?url=https://yoursite.com/your_update.php
 
-$telegram = new Telegram('5537922099:AAFFTD3OJAtreMx2i6QfbRnDYBCkHF1zWIU');
+$telegram = new Telegram(TOKEN);
 
-$chat_id = $telegram->ChatID();
-$content = array('chat_id' => $chat_id, 'text' => 'Test');
-$telegram->sendMessage($content);
+$chat_id = $telegram->ChatID() ?? '';
+$text = $telegram->Text() ?? '';
 
 file_put_contents(__DIR__.'/log.txt', print_r($telegram, 1));
 
