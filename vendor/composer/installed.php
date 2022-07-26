f31e8a6c75cebcedafb986a4991e35080e87a0f5
Orunov Sultan <sultan.orunov@mail.ru>
2022-07-27 01:19:50 +0500

    УстановÐил ÐбиÐблиотеку GoogleTranslateForFree

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2e6dda4cb2546006bc96c94bde04ceb03207ffaf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2e6dda4cb2546006bc96c94bde04ceb03207ffaf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'dejurin/php-google-translate-for-free' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'db5e3d0ac66e711dc41ed59618595aa3e3a7e475',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dejurin/php-google-translate-for-free',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
