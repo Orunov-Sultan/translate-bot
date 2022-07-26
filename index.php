2e6dda4cb2546006bc96c94bde04ceb03207ffaf
Orunov Sultan <sultan.orunov@mail.ru>
2022-07-27 01:01:57 +0500

    ЗаписываеÑмÐ даннфые пользÑоватеСделал переключение яозыкÐов

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ $telegram = new Telegram(TOKEN);
 $chat_id = $telegram->ChatID() ?? '';
 $text = $telegram->Text() ?? '';
 $first_name = $telegram->FirstName();
-
+$callback_message = $telegram->Callback_Message() ?? '';
+$callback_data = $telegram->Callback_Data() ?? '';
 
 if ($text == '/start' || $text == 'start'){
 
@@ -23,21 +24,54 @@ if ($text == '/start' || $text == 'start'){
         $check = $data['lang'];
     }
 
-    $option = [
-        [
-            $telegram->buildInlineKeyBoardButton("en", '', 'en'),
-            $telegram->buildInlineKeyBoardButton("ru", '', 'ru')
-        ]
-    ];
-    $keyb = $telegram->buildInlineKeyBoard($option);
+    $keyb = $telegram->buildInlineKeyBoard(getKeyboard($check));
+
     $content = [
         'chat_id' => $chat_id,
         'reply_markup' => $keyb,
         'text' => "Оставьте отмеченный язык для перевода с него или выберите другой"
     ];
     $telegram->sendMessage($content);
+}elseif (!empty($callback_message)){
+
+    foreach ($callback_message['reply_markup']['inline_keyboard'][0] as $item){
+        if ($item['text'] == $callback_data){
+            updateChat($callback_message['chat']['id'], $callback_data);
+
+            $response = $telegram->answerCallbackQuery([
+                'callback_query_id' => $telegram->Callback_ID(),
+                //        'text' => "Язык перевода: {$telegram->Callback_Data()}"
+            ]);
+
+            $keyb = $telegram->buildInlineKeyBoard(getKeyboard($callback_data));
+
+            $content = [
+                'chat_id' => $callback_message['chat']['id'],
+                'reply_markup' => $keyb,
+                'text' => "Можете вводить текст для перевода с выбранного языка"
+            ];
+            $telegram->sendMessage($content);
+            break;
+        }
+    }
+    $response = $telegram->answerCallbackQuery([
+        'callback_query_id' => $telegram->Callback_ID(),
+        'text' => "Это уже активный язык"
+    ]);
 }
 
-
 file_put_contents(__DIR__.'/log.txt', print_r($telegram, 1));
+file_put_contents(__DIR__.'/call.txt', print_r($callback_message['reply_markup']['inline_keyboard'][0], 1));
+
+
+function getKeyboard($lang){
+    global $telegram;
+
+    return [
+        [
+            $telegram->buildInlineKeyBoardButton($lang == "en" ? "en 🗸" : "en", '', 'en'),
+            $telegram->buildInlineKeyBoardButton($lang == "ru" ? "ru 🗸" : "ru", '', 'ru')
+        ]
+    ];
+}
 
